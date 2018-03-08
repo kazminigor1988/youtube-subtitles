afada1f51a7aa81c7904f5aabe68e554ad44cb12
Igor Kazmin <igor.kazmin@auroraglobal.com>
2018-03-08 12:26:03 +0200

    load sub file wrote

--- app-back/index.js
+++ app-back/index.js
@@ -3,17 +3,25 @@ const path          = require('path');
 
 const app = express();
 
+const YoutubeVideoSubFileLoader = require('./services/YoutubeVideoSubFileLoader');
+
+// todo change way for web files
 app.get('/js/index.js', (req, res) => {
-    res.sendFile(path.join(__dirname, './js/index.js'));
+    res.sendFile(path.join(__dirname, '../js/index.js'));
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './dist/index.html'));
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 app.get('/get', async (req, res) => {
     const videoUrl = req.query.link;
 
+    const youtubeVideoSubFileLoader = new YoutubeVideoSubFileLoader();
+
+    const subFilePath = youtubeVideoSubFileLoader.load(videoUrl);
+
+    res.status(200).send(result);
 });
 
 app.listen(3000);
--- app-back/services/YoutubeVideoSubFileLoader.js
+++ app-back/services/YoutubeVideoSubFileLoader.js
@@ -2,21 +2,43 @@ const youtube = require('youtube-dl');
 
 const path          = require('path');
 const { promisify } = require('util');
+const fs            = require('fs');
 
 const options = {
     auto: false,
     all:  false,
     lang: 'en',
-    cwd:  path.join(__dirname, './tmp'),
+    // todo extract path to config, need creation some dir for avoid conflicts for file with same name
+    cwd:  path.join(__dirname, '../../tmp'),
 };
 
-export default class YoutubeVideoSubFileLoader {
-    contructor() {
+class YoutubeVideoSubFileLoader {
+    constructor() {
         this._loadSubFileAsync = promisify(youtube.getSubs);
         this._loadOptions      = options;
     }
 
+    /**
+     * @param {string} videoLink
+     */
     async load(videoLink) {
-        const [ subFile ] = await this._loadSubFileAsync(videoLink, options);
+        let subFilePath;
+
+        try {
+            const [subFileName] = await this._loadSubFileAsync(videoLink, this._loadOptions);
+
+            subFilePath = path.join(this._loadOptions.cwd, subFileName);
+        } catch (error) {
+            console.log(error);
+            throw new Error('Can not load subtitle');
+        }
+
+        if (!fs.existsSync(subFilePath)) {
+            throw new Error('Subtitle file path is not exists');
+        }
+
+        return subFilePath;
     }
 }
+
+module.exports = YoutubeVideoSubFileLoader;
